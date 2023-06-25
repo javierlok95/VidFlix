35bfc4e011f7a2abfd2b650491fa54de4d3f2fa7
javierlok95 <javier.lok.chan@gmail.com>
2023-06-25 15:32:21 -0400

    deleting some modules

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const winston = require('winston');
 const express = require('express');
+const mongoose = require('mongoose');
+require('dotenv').config()
 const app = express();
 
 //Single responsibility Principle in Practice
 require('./startup/logging')();
 require('./startup/routes')(app);
-require('./startup/db')();
+//require('./startup/db')();
 require('./startup/config')();
 require('./startup/validation')();
 require('./startup/prod')(app);
@@ -13,8 +15,22 @@ require('./startup/prod')(app);
 //PORT an environment variable is basically a variable that is part of the environment in which a process runs. Its value is set outside this application
 //Proper Way to assign your port in your application
 const port = process.env.PORT || 3000;
-const server = app.listen(port, () => {
-    winston.info(`Listening on port ${port}...`);
+
+mongoose.set('strictQuery', false);
+const connectDB = async () => {
+    try {
+        const conn = await mongoose.connect(process.env.MONGO_URI);
+        console.log(`MongoDB Connected: ${conn.connection.host}`);
+    } catch (error) {
+        console.log(error);
+        process.exit(1);
+    }
+}
+
+const server = connectDB().then(() => {
+    app.listen(port, () => {
+        winston.info(`Listening on port ${port}...`);
+    });
 });
 
 module.exports = server;
